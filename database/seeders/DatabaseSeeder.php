b756373ba2e731b400f97e680f53b29795e8536c
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-01-23 19:10:12 -0300

    feat: first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => config('app.admin.name'),
+            'email' => config('app.admin.email'),
+            'password' => config('app.admin.password'),
+            'is_admin' => true,
+        ]);
+    }
+}
