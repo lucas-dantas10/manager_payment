960a64e1fd3f715fe6e3131adb8c3814ef7072dd
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-01-25 15:27:52 -0300

    feat: adicionado criação de Balance do usuario e 30 transações fakes para testes do grafico

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Balance;
+use App\Models\Transaction;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        $user = User::create([
             'name' => config('app.admin.name'),
             'email' => config('app.admin.email'),
             'password' => config('app.admin.password'),
             'is_admin' => true,
         ]);
+
+        Balance::create([
+            'user_id'=> $user->id,
+            'total_amount' => 0.00,
+            'created_by' => $user->id,
+            'updated_by' => $user->id,
+            'created_at' => now(),
+        ]);
+
+        Transaction::factory()->count(30)->create();
     }
 }
