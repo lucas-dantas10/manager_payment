2c1368a913dfb57c45bbeef4a2e60381fb6e6cc5
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-01-25 15:29:04 -0300

    feat: adicionado metodo booted para quaando criar transação retirar ou adicionar valor no Balance

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use TransactionTypes;
 
 class Transaction extends Model
 {
@@ -12,5 +13,23 @@ class Transaction extends Model
     public function user()
     {
         return $this->hasOne(User::class);
+    }
+
+    protected static function booted(): void
+    {
+        static::created(function (Transaction $transaction) {
+            $balance = Balance::where('user_id', auth()->user()->id)->first();
+
+            if ($transaction->type == TransactionTypes::EXPENSE->value) {
+                $balance->update([
+                    'total_amount' =>  $balance->total_amount - $transaction->amount,
+                ]);
+            } else if ($transaction->type == TransactionTypes::INCOME->value) {
+                $balance->update([
+                    'total_amount' =>  $balance->total_amount + $transaction->amount,
+                ]);
+            }
+            
+        });
     }
 }
