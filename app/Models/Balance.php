c0f2c28ec7c017a40554008e4da2d32055fd8803
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-01-24 22:50:08 -0300

    feat: adicionado os fillables

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -9,6 +9,8 @@ class Balance extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['user_id', 'created_by', 'total_amount', 'updated_by', 'created_at', 'updated_at'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
