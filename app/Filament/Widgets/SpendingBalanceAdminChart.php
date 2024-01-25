39baeaf2db8cae086005b3be6e4deb61c15ab506
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-01-25 15:31:23 -0300

    feat: adicionado grafico de total de transações no mes de todos os usuarios, apenas admin tem acesso

---
+++ app/Filament/Widgets/SpendingBalanceAdminChart.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Transaction;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class SpendingBalanceAdminChart extends ChartWidget
+{
+    protected static ?string $heading = 'Transaçoes realizadas por Mês';
+
+    protected static ?string $pollingInterval = '15s';
+
+    protected static string $color = 'info';
+
+    protected function getData(): array
+    {
+        $query = Transaction::query();
+
+        if ($this->filter !== null && $this->filter !== '') {
+            $query = Transaction::where('type', $this->filter);
+        }
+
+        $data = Trend::query($query)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Transações realizadas por mês',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getFilters(): ?array
+    {
+        return [
+            '' => 'Todos',
+            'expense' => 'Gastos',
+            'income' => 'Ganhos',
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+
+    public static function canView(): bool
+    {
+        return auth()->user()->is_admin;
+    }
+}
